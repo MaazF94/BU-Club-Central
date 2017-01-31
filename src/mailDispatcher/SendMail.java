790bec46473b36b87af9072b96a385087e424b58
Raza Mohammed <razakdash@gmail.com>
2017-01-30 20:56:42 -0600

    added e-mail functionality, needs to implemented with registration.

---
+++ src/mailDispatcher/SendMail.java
@@ -0,0 +1,55 @@
+package mailDispatcher;
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class SendMail {
+	public static void email(String fromEmail, String username, String password, String toEmail, String subject,
+			String content) {
+		//set properties to send email by
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+
+		Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		});
+		
+		try {
+	         // Create a default MimeMessage object.
+	         Message message = new MimeMessage(session);
+
+	         // Set From: header field of the header.
+	         message.setFrom(new InternetAddress(fromEmail));
+
+	         // Set To: header field of the header.
+	         message.setRecipients(Message.RecipientType.TO,
+	         InternetAddress.parse(toEmail));
+
+	         // Set Subject: header field
+	         message.setSubject(subject);
+
+	         // Now set the actual message
+	         message.setText(content);
+
+	         // Send message
+	         Transport.send(message);
+
+	         System.out.println("Sent message successfully...");
+
+	      } catch (MessagingException e) {
+	            throw new RuntimeException(e);
+	      }
+	}
+}
