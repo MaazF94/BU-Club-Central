3c6af7013c5003b580e4e24a389de59f2d381c5a
Ian Birkholz <ian.birkholz@gmail.com>
2017-03-09 23:15:20 -0600

    finished edit event and comments

---
+++ src/servlets/EditCommentServlet.java
@@ -0,0 +1,52 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import edu.ben.bu_club_central.daos.CommentDao;
+
+/**
+ * Servlet implementation class EditCommentServlet
+ */
+@WebServlet("/EditCommentServlet")
+public class EditCommentServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EditCommentServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/jsp/EditComment.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		editComment(Integer.parseInt(request.getParameter("commentid")), request.getParameter("comment"));
+		response.sendRedirect("BoardMemberDashBoard");
+	}
+
+	private void editComment(int commentID, String comment) {
+		CommentDao cDao = new CommentDao();
+		cDao.editComment(commentID, comment);
+	}
+	
+	
+	
+	
+	
+	
+}
---
+++ src/servlets/EditEventServlet.java
@@ -0,0 +1,52 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import edu.ben.bu_club_central.daos.EventsDao;
+
+/**
+ * Servlet implementation class EditEventServlet
+ */
+@WebServlet("/EditEventServlet")
+public class EditEventServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EditEventServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.getRequestDispatcher("/WEB-INF/jsp/EditEvent.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		editEvent(Integer.parseInt(request.getParameter("eventID")), request.getParameter("eventName"), request.getParameter("description"), request.getParameter("location"), Integer.parseInt(request.getParameter("rsvp_count")));
+		response.sendRedirect("BoardMemberDashBoard");
+		
+	}
+
+	private void editEvent(int idevent, String event_name, String description, String location, int rsvp_count) {
+		EventsDao eDao = new EventsDao();
+		eDao.editEvent(idevent, event_name, description, location, rsvp_count);
+		
+		
+	}
+	
+	
+}
