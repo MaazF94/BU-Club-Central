235aae018be805d51917327c6e70c79bb11d6e34
Raza Mohammed <razakdash@gmail.com>
2017-04-24 00:21:14 -0500

    changed username for database connection back to bu-user

--- src/edu/ben/bu_club_central/daos/DatabaseConnection.java
+++ src/edu/ben/bu_club_central/daos/DatabaseConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-	private String username = "root";
+	private String username = "bu-user";
 	private String password = "root";
 	private String url ="jdbc:mysql://localhost:3306/bu_club_central";
 
